aa070e3250feb1d313690fc70914090bf70f07dc
Dhruv.k <dhruv.k@digitelenetworks.com>
2023-12-21 12:05:20 +0530

    Spring boot 3 JWT 0.12.3 Implementation

--- SpringBootTokenEx/src/main/java/org/cb/App.java
+++ SpringBootTokenEx/src/main/java/org/cb/App.java
@@ -1,13 +1,48 @@
 package org.cb;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
- * Hello world!
- *
+ * JWT Token api implementation
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String secret = "D#r^v";
+        UserRs user = new UserRs(101,"Dhruvjava", "Dhruv@gmail.com", "DT11184", "9149175183");
+        String userstr = mapper.writeValueAsString(user);
+        String token = Jwts.builder().setId("iwuryrwi638").setSubject("Dhruv")
+                        .setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(
+                                        System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(2)))
+                        .setIssuer("DT").signWith(SignatureAlgorithm.HS256, secret.getBytes())
+                        .claim("user", userstr)
+                        .compact();
+
+        System.out.println("Token : " + token);
+        Claims claims = Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token)
+                        .getBody();
+        String userdt = claims.get("user", String.class);
+
+        System.out.println(userdt);
+
+        UserRs rs = mapper.readValue(userdt, UserRs.class);
+
+
+        System.out.println("ID : "+claims.getId());
+        System.out.println("Issuer : "+claims.getIssuer());
+        System.out.println("Subject : "+claims.getSubject());
+        System.out.println("Issue At : "+claims.getIssuedAt());
+        System.out.println("Expiration : "+claims.getExpiration());
+        System.out.println("user id : "+rs.getId());
+        System.out.println("user name : "+rs.getUsername());
+        System.out.println("user email : "+rs.getEmail());
+        System.out.println("user emp id : "+rs.getEmpId());
+        System.out.println("user mobile : "+rs.getMobile());
     }
 }
--- SpringBootTokenEx/src/main/java/org/cb/UserRs.java
+++ SpringBootTokenEx/src/main/java/org/cb/UserRs.java
@@ -1,2 +1,42 @@
-package org.cb;public record UserRs() {
+package org.cb;
+
+import java.io.Serializable;
+
+public class UserRs implements Serializable {
+    Integer id;
+    String username;
+    String email;
+    String empId;
+    String mobile;
+
+    public UserRs(Integer id, String username, String email, String empId, String mobile) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.empId = empId;
+        this.mobile = mobile;
+    }
+
+    public UserRs() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
 }
